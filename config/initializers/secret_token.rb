c618adb1f063508cf13efe9b0be3e1f092e1741a
Yassine Bentaieb <ybentaieb@gmail.com>
2013-06-02 15:27:57 +0200

    Created a rails pap and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'd5e61038f35e3b9b1a29119830e822ce475459a55de47d7bb0f49f37623668204f3ca15117df36bc3954b839493499c587dfc3ba075161f873d5e686e66aa997'
